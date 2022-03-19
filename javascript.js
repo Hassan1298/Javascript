74e761039e7fadbaa0cb1df282a62ba6b4ba8b08
Hassan <e.g.hassan42@gmail.com>
2022-03-19 17:13:52 +0500

    first commit

---
+++ javascript.js
@@ -0,0 +1,50 @@
+// alert("Hello World");
+
+// var name = "Hassan Khan";
+
+// document.write(" My Name is : "  + name);
+
+// var message;
+// message = "Welcome to My Website "
+
+// document.write("This is my : " + message);
+
+
+// var message;
+// message = "Welcome to My Website ";
+
+// console.log("This is my : " + message)
+
+// alert("Hello \n World");
+
+// alert("Hello \t World");
+
+// var name = prompt("Enter Your Name : ");
+
+// document.write("My Name is : " + name);
+
+// var user = prompt("Enter Your Name");
+// var password = prompt("Enter Your Password");
+
+//  if(user == "Hassan" || password == "1234"){
+//      alert("login Successfully")
+//  }
+//  else(
+//      alert("User Name or Password is Incorrect")
+//  )
+
+function ok() {
+    var first = document.getElementById("name");
+var last = document.getElementById("pass");
+
+if(first == "Hassan" || last == "khan"){
+    alert("login Successfully")
+}
+else(
+    alert("Enter Correct Name")
+)};
+
+var 
+
+
+
